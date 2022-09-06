a9a916daf1a7d2bcd7eb24408681a38dcec033e3
Ritwik Tiwari <ritwiktiwari24@gmail.com>
2022-09-05 22:47:38 -0400

    ritwiktiwari cleanup

--- tests/unit/test_num.py
+++ tests/unit/test_num.py
@@ -1,20 +1,23 @@
-import unittest
+from unittest import TestCase
 
 from src.Num import Num
 
 
-class TestNum(unittest.TestCase):
+class TestNum(TestCase):
     def setUp(self) -> None:
         self.num: Num = Num()
         for i in range(1, 101):
             self.num.add(i)
-        self.div: float = self.num.div()
-        self.mid: int = self.num.mid()
+
+    def test_add(self) -> None:
+        assert self.num.n == 100
 
     def test_div(self) -> None:
+        self.div: float = self.num.div()
         assert self.div > 30.5
         assert self.div < 32
 
     def test_mid(self) -> None:
+        self.mid: int = self.num.mid()
         assert self.mid <= 52
         assert self.mid >= 50
