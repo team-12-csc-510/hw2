2e004f87673204f33b0aa5a2b398e23609ca2e81
Saksham <sthakur5@ncsu.edu>
2022-09-05 23:51:25 -0400

    Fixed pre commit checks

--- src/cli.py
+++ src/cli.py
@@ -1,37 +1,40 @@
-import misc
 import sys
 
-help = """CSV : summarized csv file 
-(c) 2022 Tim Menzies <timm@ieee.org> BSD−2 license 
+import misc
 
-USAGE: lua seen.lua [OPTIONS] 
+help = """CSV : summarized csv file
+(c) 2022 Tim Menzies <timm@ieee.org> BSD−2 license
 
-OPTIONS: 
- -e --eg          start-up example                         = nothing 
- -d --dump        on test failure, exit with stack dump    = false 
- -f --file        file with csv data                       = ../data/auto93.csv 
- -h --help        show help                                = false  
- -n --nums        number of nums to keep                   = 512 
- -s --seed        random number seed                       = 10019 
+USAGE: lua seen.lua [OPTIONS]
+
+OPTIONS:
+ -e --eg          start-up example                         = nothing
+ -d --dump        on test failure, exit with stack dump    = false
+ -f --file        file with csv data                       = ../data/auto93.csv
+ -h --help        show help                                = false
+ -n --nums        number of nums to keep                   = 512
+ -s --seed        random number seed                       = 10019
  -S --seperator   feild seperator                          = ,]]"""
- 
+
+
 def cli(arg_dict):
     keys = list(arg_dict.keys())
     for key in keys:
         v = arg_dict[key]
         usr_args_ls = sys.argv
         for ind in range(len(usr_args_ls)):
-            if usr_args_ls[ind] == "-" + key[0] or usr_args_ls[ind] == "--" + key:
-                if type(v).__name__ == 'bool':
+            val = usr_args_ls[ind]
+            if val == "-" + key[0] or val == "--" + key:
+                if type(v).__name__ == "bool":
                     v = not v
                 else:
-                    v = usr_args_ls[ind+1]
-                    
+                    v = usr_args_ls[ind + 1]
+
             arg_dict[key] = v
-                                    
+
+
 misc.default_args(help, misc.the)
 print("Before:", misc.the)
 cli(misc.the)
 print(sys.argv)
 print("After:", misc.the)
-
--- src/misc.py
+++ src/misc.py
@@ -1,11 +1,13 @@
 import re
+from typing import Dict
 
 # Data Structure which holds all the default arguments
-the = {}
+the: Dict = {}
 
-# Identifies the input string to be a number ; int ; boolean or simply a  string
+
+# Identifies the input string to be a number
+#  int , boolean or simply a  string
 def coerce(in_str):
-    
     if in_str == "true":
         return True
     elif in_str == "false":
@@ -14,26 +16,26 @@ def coerce(in_str):
     elif in_str.isnumeric():
         return int(in_str)
     # checks if in_str is a non-integer number
-    elif re.search("[-+]?\d*\.?\d+(?:[eE][-+]?\d+)?$", in_str) != None:
+    elif re.search(r"[-+]?\d*\.?\d+(?:[eE][-+]?\d+)?$", in_str) is not None:
         return float(in_str)
     else:
         return in_str
 
-# Updates the variable     
-def default_args(def_str , arg_dict):
-    options = re.findall(r"\n [-][\S]+[\s]+[-][-]([\S]+)[^\n]+= ([\S]+)" , def_str)
+
+# Updates the variable
+def default_args(def_str, arg_dict):
+    regex = r"\n [-][\S]+[\s]+[-][-]([\S]+)[^\n]+= ([\S]+)"
+    options = re.findall(regex, def_str)
     for option in options:
         arg_dict[option[0]] = coerce(option[1])
-        
+
+
 # Our implementation of the custom print of a dictionary
 def my_print(my_dict):
-    
     print_str = "{"
     keys = list(my_dict.keys())
     for key in keys:
         print_str += ":" + str(key) + " " + str(my_dict[key])
-        
+
     print_str += "}"
     print(print_str)
-
-
