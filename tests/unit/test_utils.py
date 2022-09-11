40c165f490720875e6f7faf600515d3480723c7a
Naman Bhagat <namanbhagat@Namans-Air.lan>
2022-09-11 18:48:31 -0400

    Created test stats function

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,6 +1,9 @@
+import os
 from src import utils
 from src.num import Num
 from src.utils import custom_assert_equals
+from src import Data
+from src.utils import o
 
 
 def test_the() -> None:
@@ -12,3 +15,30 @@ def test_big_num() -> None:
     for i in range(1, 1001):
         num.add(i)
     return custom_assert_equals(len(num.has), 512, "Big num error")
+
+
+# Print some stats on columns.
+def test_stats() -> None:
+    
+    def helper_stats_div(col):
+        return col.div()
+    
+    def helper_stats_mid(col):
+        return col.mid()
+
+    # to be filled later
+    csv_file_path = os.path.join(os.path.abspath(os.curdir), "../../data/")
+    csv_file_name = "auto93.csv"
+    print(csv_file_path)
+    # data = Data.Data(csv_file_name)
+    # div = helper_stats_div
+    # mid = helper_stats_mid
+    # print("xmid", o(data.stats(2, data.cols.x, mid)))
+    # print("xdiv", o(data.stats(3, data.cols.x, div)))
+    # print("ymid", o(data.stats(2, data.cols.y, mid)))
+    # print("ydiv", o(data.stats(3, data.cols.y, div)))
+    return True
+
+    
+    
+    
