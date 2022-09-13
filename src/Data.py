756d1ce3b6b489819a6256fc3593e12aeb3a7d3e
ssingh54 <ssingh54@ncsu.edu>
2022-09-12 23:41:48 -0400

    update add Data.py commit

---
+++ src/Data.py
@@ -0,0 +1,58 @@
+from cols import Cols
+import utils
+from Row import Row
+
+
+class Data:
+    def __init__(self, src):
+        self.cols = None
+        self.rows = []
+        if isinstance(src, str):
+            self.csv(src)
+        else:
+            if src is None:
+                src = []
+            for x in range(src):
+                self.add(self.rows)
+
+    def csv(self, fname: str):
+        with open(fname, "r+") as input_file:
+            for line in input_file:
+                l = line.replace('\n', '').rstrip().split(',')
+                t = []
+                for i in l:
+                    t.append(utils.coerce(i))
+                self.add(t.copy())
+                # print(t)
+
+    def add(self, xs):
+        if not self.cols:
+            self.cols = Cols(xs)
+
+        else:
+            row = Row(xs)
+            self.rows.append(row.cells)
+            for td in self.cols.x:
+                td.add(row.cells[td.at])
+
+            for td in self.cols.y:
+                td.add(row.cells[td.at])
+
+    def stat(self, places, showCols, fun='mid'):
+        if showCols is None:
+            showCols = self.cols.y
+        t = []
+        for col in showCols:
+            v = getattr(col, fun)()
+            if isinstance(v, int):
+                v = utils.rnd(v, places)
+            t[col.name] = v
+        return t
+
+
+
+
+
+
+
+
