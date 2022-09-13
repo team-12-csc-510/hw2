80e36cdec1964c20b3a158e323a652dd5dc390fa
Naman Bhagat <namanbhagat@Namans-Air.lan>
2022-09-12 23:53:33 -0400

    updated sys path to import src module

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,10 +1,14 @@
 import os
+import sys
+abs_file_path = os.path.abspath(__file__)
+path_ls = abs_file_path.split("/")[:-3]
+new_path = "/".join(path_ls)
+sys.path.append(new_path)
 from src import utils
 from src.num import Num
 from src.utils import custom_assert_equals
-from src import Data
+# from src import Data
 from src.utils import o
-
 
 def test_the() -> None:
     return custom_assert_equals(utils.the.get("nums"), 512, "Utils the error")
@@ -26,19 +30,14 @@ def test_stats() -> None:
     def helper_stats_mid(col):
         return col.mid()
 
-    # to be filled later
-    csv_file_path = os.path.join(os.path.abspath(os.curdir), "../../data/")
     csv_file_name = "auto93.csv"
+    csv_file_path = os.path.join("/".join(os.path.abspath(__file__).split("/")[:-3]), "data", csv_file_name) 
     print(csv_file_path)
-    # data = Data.Data(csv_file_name)
-    # div = helper_stats_div
-    # mid = helper_stats_mid
-    # print("xmid", o(data.stats(2, data.cols.x, mid)))
-    # print("xdiv", o(data.stats(3, data.cols.x, div)))
-    # print("ymid", o(data.stats(2, data.cols.y, mid)))
-    # print("ydiv", o(data.stats(3, data.cols.y, div)))
+    data = Data.Data(csv_file_name)
+    div = helper_stats_div
+    mid = helper_stats_mid
+    print("xmid", o(data.stats(2, data.cols.x, mid)))
+    print("xdiv", o(data.stats(3, data.cols.x, div)))
+    print("ymid", o(data.stats(2, data.cols.y, mid)))
+    print("ydiv", o(data.stats(3, data.cols.y, div)))
     return True
-
-    
-    
-    
